eb27ef316320ceebefb2447c0f37110c50074e2d
Trọng Nghĩa Ngô <nghiango262@gmail.com>
2020-03-05 06:28:17 +0700

    Tạo nhánh login để thực hiện chức năng login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/App.scss";
+const reactLogo = require("./assets/img/react_logo.svg");
+import "./assets/scss/App.scss";
 
 class App extends React.Component<{}, undefined> {
     public render() {
         return (
             <div className="app">
-                <h1>Hello World!</h1>
+                <h1>Cais quajn !</h1>
                 <p>Foo to the barz</p>
                 <img src={reactLogo.default} height="480"/>
             </div>
